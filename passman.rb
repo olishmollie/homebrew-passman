e074a729cf2c15aa46cdf6debdf30c7d784dfcbe
ajbond <olishmollie@gmail.com>
2018-06-03 01:03:28 -0500

    v0.5.9

--- passman.rb
+++ passman.rb
@@ -1,8 +1,8 @@
 class Passman < Formula
     desc "A UNIX password manager"
     homepage "https://github.com/olishmollie/passman"
-    url "https://github.com/olishmollie/passman/archive/v0.5.8.tar.gz"
-    sha256 "5991c6007b4c200d5f91f85ebe182f4ac8b57468fc7363e87922cc8a6329d4e0"
+    url "https://github.com/olishmollie/passman/archive/v0.5.9.tar.gz"
+    sha256 "bb43f10f5c58e2a83dbd924f2e2cfbddb07ac673411bf18cd6842ae25e2cd2dc"
 
     def install
         bin.install "bin/passman"
