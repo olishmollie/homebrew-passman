285644b7f37118030b4b3532fd34c5f376d67a06
ajbond <olishmollie@gmail.com>
2018-06-29 14:57:15 -0500

    Update hash

--- fig.rb
+++ fig.rb
@@ -2,7 +2,7 @@ class Fig < Formula
     desc "A stack based expression parser"
     homepage "https://github.com/olishmollie/fig"
     url "https://github.com/olishmollie/fig/archive/v0.0.4.tar.gz"
-    sha256 "4550df2055c2eadf377224e5a8da499daf1723599df1501ddcd0dd21f3d3ee4b"
+    sha256 "9cc21b0ce6f35ce26417928d954b4b9168ba6b0f9bb5c54c2877fd62730e58e9"
 
     def install
         bin.install "bin/fig"
