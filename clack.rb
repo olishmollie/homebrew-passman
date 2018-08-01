c6af2f6353e354d016fcb3c378e276c36e525631
ajbond <olishmollie@gmail.com>
2018-08-01 14:44:08 -0500

    update clack sha

--- clack.rb
+++ clack.rb
@@ -2,7 +2,7 @@ class Clack < Formula
     desc "A stack based infix expression parser"
     homepage "https://github.com/olishmollie/clack"
     url "https://github.com/olishmollie/clack/archive/v0.0.6.tar.gz"
-    sha256 "832e24184160ca6ed1f9a2f29ae61099c6370bc888eeb93c308fefb097750d38"
+    sha256 "61b530a9a4ecc02fb495a9027626226db050e2d50e8d874be12c39a845bd7458"
 
     def install
         bin.install "bin/clack"
