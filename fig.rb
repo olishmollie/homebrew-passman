445ef73a2382230b01924dff3ef48dd7ab812288
ajbond <olishmollie@gmail.com>
2018-08-01 15:49:15 -0500

    change tar location

--- fig.rb
+++ fig.rb
@@ -1,7 +1,7 @@
 class Fig < Formula
     desc "An implementation of a minimal lisp dialect"
     homepage "https://github.com/olishmollie/fig"
-    url "https://github.com/olishmollie/clack/archive/v0.1.tar.gz"
+    url "https://github.com/olishmollie/fig/archive/v0.1.tar.gz"
     sha256 "61b530a9a4ecc02fb495a9027626226db050e2d50e8d874be12c39a845bd7458"
 
     def install
