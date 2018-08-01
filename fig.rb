27a821cdbb51cd2adbf502392c641f6926632ac3
ajbond <olishmollie@gmail.com>
2018-08-01 18:39:27 -0500

    try adding lib files to a liib folder

--- fig.rb
+++ fig.rb
@@ -2,10 +2,11 @@ class Fig < Formula
     desc "An implementation of a minimal lisp dialect"
     homepage "https://github.com/olishmollie/fig"
     url "https://github.com/olishmollie/fig/archive/v0.1.tar.gz"
-    sha256 "61b530a9a4ecc02fb495a9027626226db050e2d50e8d874be12c39a845bd7458"
+    sha256 "221134876160e5c3d8ebb69bc1d91703a2d3c63fe9ca32f953d11a93b7f5713b"
 
     def install
         bin.install "bin/fig"
-	bin.install "bin/lib.fig"
+	lib.install "lib/lib.fig"
+	lib.install "lib/tests.fig"
     end
 end
