55d0f10d174c4d4b57ee5d3a5c426ac5cefd106b
ajbond <olishmollie@gmail.com>
2019-05-07 08:05:03 -0500

    another try

--- fig.rb
+++ fig.rb
@@ -2,7 +2,7 @@ class Fig < Formula
     desc "An implementation of a minimal lisp dialect"
     homepage "https://github.com/olishmollie/fig"
     url "https://github.com/olishmollie/fig/archive/v0.1.tar.gz"
-    sha256 "ae820b50315378b5f5e67a184b63d7b6263b44d8bb5e6a48180abc237c769f66"
+    sha256 "1a69e601a3c31fa9bdc4ed7ccd49fc5fafa949a2474df4c4ad318ab2686aa924"
 
     def install
         bin.install "bin/fig"
