157072da8e0a0b241357f1a16026505cad895723
ajbond <olishmollie@gmail.com>
2019-05-07 07:56:31 -0500

    another shasum mismatch

--- fig.rb
+++ fig.rb
@@ -2,7 +2,7 @@ class Fig < Formula
     desc "An implementation of a minimal lisp dialect"
     homepage "https://github.com/olishmollie/fig"
     url "https://github.com/olishmollie/fig/archive/v0.1.tar.gz"
-    sha256 "0505c378c56ca148d47f0a2fe878f9a9ebfb7dba692c7d1341c6f242faafab25"
+    sha256 "ae820b50315378b5f5e67a184b63d7b6263b44d8bb5e6a48180abc237c769f66"
 
     def install
         bin.install "bin/fig"
