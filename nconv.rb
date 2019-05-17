ed2f320f950a7f13205ce3bb39a056c2d567a0c1
ajbond <olishmollie@gmail.com>
2019-05-17 15:07:10 -0500

    nconv v0.1.0

--- nconv.rb
+++ nconv.rb
@@ -2,9 +2,9 @@ class Nconv < Formula
     desc "Number CONVerter"
     homepage "https://github.com/olishmollie/nconv"
     url "https://github.com/olishmollie/nconv/archive/v0.1.0.tar.gz"
-    sha256 "cf06231d7194957bf65813de9e0d9a07386d1a8c10f521b20adc0c45bc3bcc41"
+    sha256 "cb6ceb259f50f761b2f3e5841e2fb7866bdb0c13043a480e3356cadea5060192"
 
     def install
-        bin.install "target/release/nconv"
+        bin.install "nconv"
     end
 end
