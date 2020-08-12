5f71410db76aa1c2c7fcb24d94e99dd405517469
ajbond <olishmollie@gmail.com>
2020-08-11 22:21:22 -0500

    Change executable to 'radix'

--- radix.rb
+++ radix.rb
@@ -5,6 +5,6 @@ class Radix < Formula
     sha256 "6789da415d575cfa8786390e30a837b9ef45e494046b9cf9e6d8369de8a53adc"
 
     def install
-        bin.install "nconv"
+        bin.install "radix"
     end
 end
