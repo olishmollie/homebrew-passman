fd55ccc7bbac5ee212b4564ab58577d1ea8074ff
ajbond <olishmollie@gmail.com>
2020-08-11 22:21:00 -0500

    Radix v1.0.0

---
+++ radix.rb
@@ -0,0 +1,10 @@
+class Radix < Formula
+    desc "A command line utility to change bases."
+    homepage "https://github.com/olishmollie/radix"
+    url "https://github.com/olishmollie/radix/archive/v1.0.0.tar.gz"
+    sha256 "6789da415d575cfa8786390e30a837b9ef45e494046b9cf9e6d8369de8a53adc"
+
+    def install
+        bin.install "nconv"
+    end
+end
