f908ea3933be84b883312d7ef16dd0512578ec45
ajbond <olishmollie@gmail.com>
2020-08-11 22:42:33 -0500

    Why does the sha never match??

--- radix.rb
+++ radix.rb
@@ -2,7 +2,7 @@ class Radix < Formula
     desc "A command line utility to change bases."
     homepage "https://github.com/olishmollie/radix"
     url "https://github.com/olishmollie/radix/archive/v1.0.0.tar.gz"
-    sha256 "32064105dd0dd6b100f161ea18c59be5d606ced89c138d2a39c7f9a2a86ceae5"
+    sha256 "5bdb2cf222d4081808f8056244b5b07b124f2f1692de3a3c4e93f19ee984e7c9"
 
     def install
         bin.install "radix"
